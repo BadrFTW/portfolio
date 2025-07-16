ac6a9bca7124b2c45c972135a13d880376aa92d5
Klila Badr <klilabader2@gmail.com>
2025-07-16 21:09:35 +0100

    done with the main page

---
+++ index.js
@@ -0,0 +1,10 @@
+const sideMenu= document.getElementById('sideMenu');
+const openMenu=()=>{
+    sideMenu.style.transform='translateX(-16rem)';
+
+}
+const closeMenu=()=>{
+    sideMenu.style.transform='translateX(16rem)';
+
+
+}
